0e74edae73124ffda8e646b7eaad8e0179da1734
Aigerim <aigerim@MacBook-Air-Aigerim-2.local>
2022-11-21 03:43:17 -0800

    not final, but some amendments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 choice = ['r', 's', 'p']
-count = [0, 1]
+
 def check_answer(t):
     if t == 'r':
         print("Rock")
@@ -10,45 +10,39 @@ def check_answer(t):
     elif t == 'p':
         print('Paper')
 
-def check_win(win, lose):
-    if win == 'r' and lose == 'p':
-        print(win)
-    elif win == 'p' and lose == 'r':
-        print()
-    elif win == ''
+def check_win(turn, c_turn):
+    count = [0, 0]
+    if turn == 'r' and c_turn == 's' or turn == 'p' and c_turn == 's' or turn == 's' and c_turn == 'p':
+        print('You won')
+        count[0] += 1
+    elif c_turn == 'r' and turn == 's' or c_turn == 'p' and turn == 's' or c_turn == 's' and turn == 'p':
+        print('Comp won')
+        count[1] += 1
+    elif turn == c_turn:
+        print("It is tie: ")
+    print('the score is: ', count[0], ':', count[1])
 
 def main():
-    a = 0
-    b = 0
-    while True:
-        turn = input("Please enter your turn: ('r', 's', 'p'): ").lower()
+    round = 0
+    while round < 5:
+        turn = input("please enter choose ('r', 's', 'p'): ").lower()
         if turn == 'exit':
             break
         else:
             check_answer(turn)
         c_turn = random.choice(choice)
         check_answer(c_turn)
-
-        # if turn == 'r' and c_turn == 's':
-        #     print('You win')
-        # elif turn == 'p' and c_turn == 'r':
-        #     print('You win')
-        # elif turn == 's' and c_turn == 'p':
-        #     print('You win')
-        # elif c_turn == 'r' and turn == 's':
-        #     print('comp win')
-        # elif c_turn == 'p' and turn == 'r':
-        #     print('You win')
-        # elif c_turn == 's' and turn == 'p':
-        #     print('You win')
-
-        if turn == 'r' and c_turn == 's' or turn == 'p' and c_turn == 'r' or turn == 's' and c_turn == 'p':
-            print('You win')
-            count[0] += 1
-        elif c_turn == 'r' and turn == 's' or c_turn == 'p' and turn == 'r' or c_turn == 's' and turn == 'p':
-            print('Comp win')
-            count[1] += 1
-        print('current score:', count)
-        if
+        check_win(turn, c_turn)
+        if turn not in choice:
+            print("you chose incorrectly: ")
+            continue
+        round += 1
+        if round == 5:
+            return False
+            break
+    if count[0] > count[1]:
+        print('Congrats you won the game')
+    else:
+        print('Computer won the game')
 
 main()
